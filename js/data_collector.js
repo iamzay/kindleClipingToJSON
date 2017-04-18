f6c67ac157d6881dbeb5cfd30b605707f9b0ce7c
iamzay <zayhust@gmail.com>
2017-04-18 19:26:20 +0800

    添加空格和函数名

--- js/data_collector.js
+++ js/data_collector.js
@@ -1,10 +1,10 @@
-function DataCollector(){
+function DataCollector() {
     this.bookList=[];
     this.mark=[];
 }
 
-DataCollector.prototype.collect=function(cliping){
-    var pattern=/([\s\S]*?)(- (?:您在|Your (?:Highlight|Note))[\s\S]*?\| )([\s\S]*?(?:[0-9]|1[0-2]):(?:[0-5][0-9]):(?:[0-5][0-9])( [PA]M)?)([\s\S]*?)={10}/g;
+DataCollector.prototype.collect=function collect(cliping) {
+    var pattern=/([\s\S]*?)(- (?:您在|Your (?:Highlight|Note))[\s\S]*?\| )([\s\S]*?(?:[0-9]|1[0-2]):(?:[0-5][0-9]):(?:[0-5][0-9])( [PA]M)?)([\s\S]*?)= {10}/g;
     var match=null;
     var record;
     var meta;
@@ -12,48 +12,48 @@ DataCollector.prototype.collect=function(cliping){
     // 删除cliping的\n和\r
     cliping=cliping.replace(/[\n\r\ufeff]/g,'');
 
-    while((match=pattern.exec(cliping))){
+    while((match=pattern.exec(cliping))) {
         meta=this.getTitleAuthor(match[1]);
-        record={};
+        record= {};
         record.title=meta.title;
         record.author=meta.author;
         record.position=this.getPosition(match[2]);
         record.time=this.getDate(match[3])+' '+this.getTime(match[3]);
         record.content=this.getContent(match[5]);
 
-        if(record.content.length){
+        if (record.content.length) {
             this.mark.push(record);
 
-            if(this.bookList.indexOf(record.title)===-1){
+            if (this.bookList.indexOf(record.title)===-1) {
                 this.bookList.push(record.title);
             }
         }
     }
 };
 
-DataCollector.prototype.getPosition=function(positionLine){
+DataCollector.prototype.getPosition=function getPosition(positionLine) {
     var pattern=/#(\d+)/;
     var pattern2=/(\d+)/;
     var match=pattern.exec(positionLine);
 
-    if(match){
+    if (match) {
         return match[1];
-    } else{
+    } else {
         match=pattern2.exec(positionLine);
         return !match?'':match[1];
     }
 };
 
-DataCollector.prototype.getDate=function(dateLine){
-    if(dateLine[0]==='A'){
+DataCollector.prototype.getDate=function getDate(dateLine) {
+    if (dateLine[0]==='A') {
         return this.getDateFromEng(dateLine);
-    } else{
+    } else {
         return this.getDateFromChn(dateLine);
     }
 };
 
-DataCollector.prototype.getDateFromEng=function(dateLine){
-    var map={
+DataCollector.prototype.getDateFromEng=function getDateFromEng(dateLine) {
+    var map= {
         January:'01',
         February:'02',
         March:'03',
@@ -71,7 +71,7 @@ DataCollector.prototype.getDateFromEng=function(dateLine){
     var match=pattern.exec(dateLine);
     var day='';
 
-    if(match){
+    if (match) {
         day=this.normalizeData(match[2]);
         return match[3]+'-'+map[match[1]]+'-'+day;
     }
@@ -79,13 +79,13 @@ DataCollector.prototype.getDateFromEng=function(dateLine){
     return '';
 };
 
-DataCollector.prototype.getDateFromChn=function(dateLine){
+DataCollector.prototype.getDateFromChn=function getDateFromChn(dateLine) {
     var pattern=/(\d+)年(\d+)月(\d+)日/;
     var match=pattern.exec(dateLine);
     var month='';
     var day='';
 
-    if(match){
+    if (match) {
         month=this.normalizeData(match[2]);
         day=this.normalizeData(match[3]);
         return match[1]+'-'+month+'-'+day;
@@ -94,34 +94,34 @@ DataCollector.prototype.getDateFromChn=function(dateLine){
     return '';
 };
 
-DataCollector.prototype.getTime=function(timeLine){
-    if(timeLine[timeLine.length-1]==='M'){
+DataCollector.prototype.getTime=function getTime(timeLine) {
+    if (timeLine[timeLine.length-1]==='M') {
         return this.getTimeFromEng(timeLine);
-    } else{
+    } else {
         return this.getTimeFromChn(timeLine);
     }
 };
 
-DataCollector.prototype.getTimeFromEng=function(timeLine){
+DataCollector.prototype.getTimeFromEng=function getTimeFromEng(timeLine) {
     var pattern=/([0-9]|1[0-2]):([0-5][0-9]):([0-5][0-9]) ([PA]M)?/;
     var match=pattern.exec(timeLine);
     var hour='';
 
-    if(match){
+    if (match) {
 
         // a.m转24h制
-        if(match[4]==='AM'){
+        if (match[4]==='AM') {
 
             // 若为12:00 am,转为00:00 am
-            if(match[1]==='12'){
+            if (match[1]==='12') {
                 hour='00';
-            } else{
+            } else {
                 hour=this.normalizeData(match[1]);
             }
-        } else{  // p.m转24h制
+        } else {  // p.m转24h制
             hour=match[1];
 
-            if(match[1]!=='12'){
+            if (match[1]!=='12') {
                 hour=(parseInt(match[1])+12).toString();
             }
         }
@@ -132,24 +132,24 @@ DataCollector.prototype.getTimeFromEng=function(timeLine){
     return '';
 };
 
-DataCollector.prototype.getTimeFromChn=function(timeLine){
+DataCollector.prototype.getTimeFromChn=function getTimeFromChn(timeLine) {
     var pattern=/([上下]午)([0-9]|1[0-2]):([0-5][0-9]):([0-5][0-9])/;
     var match=pattern.exec(timeLine);
     var hour='';
 
-    if(match){
+    if (match) {
 
-        if(match[1]==='上午'){
+        if (match[1]==='上午') {
 
-            if(match[2]==='12'){
+            if (match[2]==='12') {
                 hour='00';
-            } else{
+            } else {
                 hour=this.normalizeData(match[2]);
             }
-        } else{
+        } else {
             hour=match[2];
 
-            if(match[2]!=='12'){
+            if (match[2]!=='12') {
                 hour=(parseInt(match[2])+12).toString();
             }
         }
@@ -160,33 +160,33 @@ DataCollector.prototype.getTimeFromChn=function(timeLine){
     return '';
 };
 
-DataCollector.prototype.getContent=function(contentLine){
+DataCollector.prototype.getContent=function getContent(contentLine) {
     return contentLine;
 };
 
-DataCollector.prototype.getTitleAuthor=function(titleLine){
+DataCollector.prototype.getTitleAuthor=function getTitleAuthor(titleLine) {
     var cnt=1;
     var len=titleLine.length;
     var i;
     var j;
 
-    for(i=len-2;i>=0 && cnt;--i){
+    for (i=len-2;i>=0 && cnt;--i) {
 
-        if(titleLine[i]===')'){
+        if (titleLine[i]===')') {
             ++cnt;
         }
 
-        if(titleLine[i]=='('){
+        if (titleLine[i]=='(') {
             --cnt;
         }
     }
 
-    return {
+    return  {
         title:titleLine.slice(0,i),
         author:titleLine.slice(i+2,len-1)
     };
 };
 
-DataCollector.prototype.normalizeData=function(data){
+DataCollector.prototype.normalizeData=function normalizeData(data) {
     return data.length===1?'0'+data:data;
 };
