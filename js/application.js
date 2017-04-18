dfb3daa67c3b9132757467bb680ede276cace413
iamzay <zayhust@gmail.com>
2017-04-18 10:54:21 +0800

    Revert "更改初始化写法"
    This reverts commit f52de14747371d37f5a2235e9f950abb0de32ee9.

--- js/application.js
+++ js/application.js
@@ -1,65 +1,57 @@
-({
-    removeDefault:function(event){
-        event.stopPropagation();
-        event.preventDefault();
-    },
+window.onload=function(){
+    var dropbox=document.querySelector(".dropbox");
 
-    dragHandler:function(event){
-        removeDefault(event);
+    dropbox.addEventListener("dragenter",removeDefault,false);
+    dropbox.addEventListener("dragover",removeDefault,false);
+    dropbox.addEventListener("drop",dragHandler,false);
 
-        var dt=event.dataTransfer;
-        var files=dt.files;
+    test();
+};
 
-        if(/[\s\S]*.txt/.exec(files[0].name)===null){
-            alert("请放入一个txt文件");
-            return;
-        }
+function removeDefault(event){
+    event.stopPropagation();
+    event.preventDefault();
+}
 
-        processFile(files);
-    },
+function dragHandler(event){
+    removeDefault(event);
 
-    processFile:function(files){
-        var fileReader=new FileReader();
+    var dt=event.dataTransfer;
+    var files=dt.files;
 
-        fileReader.readAsText(files[0]);
-
-        fileReader.addEventListener("load",function(event){
-            var dataCollector=new DataCollector();
-
-            dataCollector.collect(event.target.result);
-
-            var jsonCreator=new JsonCreator(dataCollector);
-            jsonCreator.create();
-        });
-    },
-
-    test:function(){
-        var iframe=document.querySelector("#frmFile");
-        var cliping=iframe.contentWindow.document.body.childNodes[0].innerHTML;
-
-        var dataCollector=new DataCollector();
-        dataCollector.collect(cliping);
-
-        var bookList=dataCollector.bookList,
-            mark=dataCollector.mark;
-
-        console.log(bookList);
-        console.log(mark);
-    },
-
-    init:function(){
-        var removeDefault=this.removeDefault,
-            dragHandler=this.dragHandler,
-            test=this.test;
-
-        window.onload=function(){
-            var dropbox=document.querySelector(".dropbox");
-
-            dropbox.addEventListener("dragenter",removeDefault,false);
-            dropbox.addEventListener("dragover",removeDefault,false);
-            dropbox.addEventListener("drop",dragHandler,false);
-
-            test();
-        };
+    if(/[\s\S]*.txt/.exec(files[0].name)===null){
+        alert("请放入一个txt文件");
+        return;
     }
-}).init();
+
+    processFile(files);
+}
+
+function processFile(files){
+    var fileReader=new FileReader();
+
+    fileReader.readAsText(files[0]);
+
+    fileReader.addEventListener("load",function(event){
+        var dataCollector=new DataCollector();
+
+        dataCollector.collect(event.target.result);
+
+        var jsonCreator=new JsonCreator(dataCollector);
+        jsonCreator.create();
+    });
+}
+
+function test(){
+    var iframe=document.querySelector("#frmFile");
+    var cliping=iframe.contentWindow.document.body.childNodes[0].innerHTML;
+
+    var dataCollector=new DataCollector();
+    dataCollector.collect(cliping);
+
+    var bookList=dataCollector.bookList,
+        mark=dataCollector.mark;
+
+    console.log(bookList);
+    console.log(mark);
+}
