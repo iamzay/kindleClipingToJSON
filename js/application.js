3fd11d623249ba820812f0726a9c14d678dffd6b
iamzay <zayhust@gmail.com>
2017-04-19 10:23:47 +0800

    Revert "添加空格和函数名"
    This reverts commit f6c67ac157d6881dbeb5cfd30b605707f9b0ce7c.

--- js/application.js
+++ js/application.js
@@ -1,4 +1,4 @@
-window.onload=function() {
+ window.onload=function(){
     var dropbox=document.querySelector('.dropbox');
 
     dropbox.addEventListener('dragenter',removeDefault,false);
@@ -8,18 +8,18 @@ window.onload=function() {
     test();
 };
 
-function removeDefault(event) {
+function removeDefault(event){
     event.stopPropagation();
     event.preventDefault();
 }
 
-function dropHandler(event) {
+function dropHandler(event){
     var dt=event.dataTransfer;
     var files=dt.files;
 
     removeDefault(event);
 
-    if (!(/[\s\S]*.txt/.exec(files[0].name))) {
+    if(!(/[\s\S]*.txt/.exec(files[0].name))){
         alert('请放入一个txt文件');
         return;
     }
@@ -27,10 +27,10 @@ function dropHandler(event) {
     processFile(files[0]);
 }
 
-function processFile(file) {
+function processFile(file){
     var fileReader=new FileReader();
 
-    fileReader.addEventListener('load',function(event) {
+    fileReader.addEventListener('load',function(event){
         var dataCollector=new DataCollector();
         var jsonCreator=new JsonCreator(dataCollector);
 
@@ -42,7 +42,7 @@ function processFile(file) {
     fileReader.readAsText(file);
 }
 
-function test() {
+function test(){
     var iframe=document.querySelector('#frmFile');
     var cliping=iframe.contentWindow.document.body.childNodes[0].innerHTML;
     var dataCollector=new DataCollector();
--- js/json_creator.js
+++ js/json_creator.js
@@ -1,14 +1,14 @@
-function JsonCreator(dataCollector) {
+function JsonCreator(dataCollector){
     this.data=dataCollector;
 }
 
-JsonCreator.prototype.create=function create() {
+JsonCreator.prototype.create=function(){
     this.createFile(this.data.bookList,'bookList.json');
     this.createFile(this.data.mark,'mark.json');
 };
 
-JsonCreator.prototype.createFile=function createFile(obj,fileName) {
-    var blob=new Blob([JSON.stringify(obj,null,2)], {type:'application/json'});
+JsonCreator.prototype.createFile=function(obj,fileName){
+    var blob=new Blob([JSON.stringify(obj,null,2)],{type:'application/json'});
 
     saveAs(blob,fileName);
 };
